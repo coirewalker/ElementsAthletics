2be85d4c2f8a1165b336d30675bc77f8e173a0ee
whereiscoire <whereiscoire@gmail.com>
2016-03-29 23:07:51 -0700

    contact page gmap

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
 	    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
 	    var mapOptions = {
 	        // How zoomed in you want the map to start at (always required)
-	        zoom: 15,
+	        zoom: 14,
 
 	        // The latitude and longitude to center the map (always required)
 	        center: new google.maps.LatLng(40.678430, -73.954907), // New York
@@ -51,117 +51,6 @@ $(document).ready(function(){
 	        disableDefaultUI: true,
 	        scrollwheel: false,
 	        draggable: true,
-
-	    //     [{
-	    //         "featureType": "water",
-	    //         "elementType": "geometry",
-	    //         "stylers": [{
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 17
-	    //         }]
-	    //     }, {
-	    //         "featureType": "landscape",
-	    //         "elementType": "geometry",
-	    //         "stylers": [{
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 20
-	    //         }]
-	    //     }, {
-	    //         "featureType": "road.highway",
-	    //         "elementType": "geometry.fill",
-	    //         "stylers": [{
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 17
-	    //         }]
-	    //     }, {
-	    //         "featureType": "road.highway",
-	    //         "elementType": "geometry.stroke",
-	    //         "stylers": [{
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 29
-	    //         }, {
-	    //             "weight": 0.2
-	    //         }]
-	    //     }, {
-	    //         "featureType": "road.arterial",
-	    //         "elementType": "geometry",
-	    //         "stylers": [{
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 18
-	    //         }]
-	    //     }, {
-	    //         "featureType": "road.local",
-	    //         "elementType": "geometry",
-	    //         "stylers": [{
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 16
-	    //         }]
-	    //     }, {
-	    //         "featureType": "poi",
-	    //         "elementType": "geometry",
-	    //         "stylers": [{
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 21
-	    //         }]
-	    //     }, {
-	    //         "elementType": "labels.text.stroke",
-	    //         "stylers": [{
-	    //             "visibility": "on"
-	    //         }, {
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 16
-	    //         }]
-	    //     }, {
-	    //         "elementType": "labels.text.fill",
-	    //         "stylers": [{
-	    //             "saturation": 36
-	    //         }, {
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 40
-	    //         }]
-	    //     }, {
-	    //         "elementType": "labels.icon",
-	    //         "stylers": [{
-	    //             "visibility": "off"
-	    //         }]
-	    //     }, {
-	    //         "featureType": "transit",
-	    //         "elementType": "geometry",
-	    //         "stylers": [{
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 19
-	    //         }]
-	    //     }, {
-	    //         "featureType": "administrative",
-	    //         "elementType": "geometry.fill",
-	    //         "stylers": [{
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 20
-	    //         }]
-	    //     }, {
-	    //         "featureType": "administrative",
-	    //         "elementType": "geometry.stroke",
-	    //         "stylers": [{
-	    //             "color": "#000000"
-	    //         }, {
-	    //             "lightness": 17
-	    //         }, {
-	    //             "weight": 1.2
-	    //         }]
-	    //     }]
-	    // };
-
 
 	    // How you would like to style the map. 
 	    // This is where you would paste any style found on Snazzy Maps.
