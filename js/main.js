fa17a843aeb49e00db456ffda6f4ef5e8564e511
whereiscoire <whereiscoire@gmail.com>
2016-04-05 22:02:37 -0400

    testing fix for mobile scroll bar issue

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
 
 
 	// fix scrolling bg issue caused by search bar on Chrome and FF mobile 
-	var bg = $('.homepage-bg, .about-bg, .classes-bg, .coaches-bg, membership-bg, .contact-bg');
+	var bg = $('.homepage-bg, .about-bg, .classes-bg, .coaches-bg, .membership-bg, .contact-bg');
 	
 	$(window).resize('resizeBackground');
 	
@@ -37,10 +37,14 @@ $(document).ready(function(){
 	resizeBackground();
 
 	// fix hero text against bg on scroll mobile 
-	// var hero = jQuery('.hero-title');
+	var hero = $('.hero-title');
 
+	$(window).resize('holdText');
 
-
+	function holdText() {
+		hero.height($(window).css('height' + 55vh);
+	}
+	holdText();
 
 	// GOOGLE MAP 
 	// When the window has finished loading create our google map below
